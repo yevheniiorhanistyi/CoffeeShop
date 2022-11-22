196c3f8e5e25e7716cfddf9615bfab0523779dba
Yevhenii Orhanistyi <organistiy@gmail.com>
2022-11-22 03:48:23 +0000

    start create coffee items onclick

--- src/componets/about-groupe/about-groupe.js
+++ src/componets/about-groupe/about-groupe.js
@@ -5,8 +5,10 @@ import './about-groupe.scss';
 
 const AboutGroupe = ({title, src, p1, p2}) => {
 
+    const x = `Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`;
+
     return(
-        <div className='about-groupe'>
+        <div className='about'>
                 <div className="container text-center">
                         <div className="d-flex row align-items-center justify-content-center flex-wrap">
                             {src && <div className="col-sm-5">
@@ -16,10 +18,10 @@ const AboutGroupe = ({title, src, p1, p2}) => {
                             <div className={src ? 'col-sm-4' : 'col'}>
                                 <h2 className="about-title">{title}</h2>
                                 <CoffeeBeans color="black" />
-                                <p className="coffee-card__country">{'Country: Brasil'}</p>
-                                <p>{p1}</p>
-                                <p className="coffee-card__price">{'Price:  16.99$'}</p>
-                                <p>{p2}</p>
+                                <p className="country">Country:{'Brasil'}</p>
+                                <p className="about-text">{x}</p>
+                                <p className="price">Price:{'16.99$'}</p>
+                                <p className="about-text">{}</p>
                             </div>
                         </div>
                 </div>
